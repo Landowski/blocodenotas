5d815fd7c95d34c3034cf335b07217c2f39de125
Landowski <105506160+Landowski@users.noreply.github.com>
2024-10-20 12:58:19 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,6 +14,9 @@ const underlineBtn = document.getElementById('underlineBtn');
 const leftBtn = document.getElementById('leftBtn');
 const centerBtn = document.getElementById('centerBtn');
 const rightBtn = document.getElementById('rightBtn');
+const justifyBtn = document.getElementById('justifyBtn');
+const markerBtn = document.getElementById('markerBtn');
+const listOrderBtn = document.getElementById('listOrderBtn');
 const listBtn = document.getElementById('listBtn');
 const linkBtn = document.getElementById('linkBtn');
 const imageBtn = document.getElementById('imageBtn');
@@ -29,7 +32,7 @@ const menu = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
 const todoInput = document.getElementById("input-to-do");
 const todoList = document.getElementById("to-do");
-let localNotes = [];
+let notes = [];
 let currentNoteId = null;
 
 // Listeners iniciais
@@ -71,21 +74,21 @@ logo.addEventListener("click", () => {
 
 // Início do local storage
 function initializeLocalStorage() {
-    if (!localStorage.getItem('localNotes')) {
-        localStorage.setItem('localNotes', JSON.stringify([]));
+    if (!localStorage.getItem('notes')) {
+      localStorage.setItem('notes', JSON.stringify([]));
     }
-    if (!localStorage.getItem('localTodos')) {
-        localStorage.setItem('localTodos', JSON.stringify([]));
+    if (!localStorage.getItem('todos')) {
+      localStorage.setItem('todos', JSON.stringify([]));
     }
     syncNotes();
 }
 
 function syncNotes() {
-    localNotes = JSON.parse(localStorage.getItem('localNotes')) || [];
+    notes = JSON.parse(localStorage.getItem('notes')) || [];
 }
 
 function saveNotes() {
-    localStorage.setItem('localNotes', JSON.stringify(localNotes));
+    localStorage.setItem('notes', JSON.stringify(notes));
 }
 
 function applyStoredTheme() {
@@ -99,34 +102,34 @@ function applyStoredTheme() {
 
 // Lista de tarefas
 function addTodo(text) {
-    const todos = JSON.parse(localStorage.getItem('localTodos')) || [];
-    const newOrder = todos.length > 0 ? Math.max(...todos.map(t => t.localToDoOrdem)) + 1 : 0;
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const newOrder = todos.length > 0 ? Math.max(...todos.map(t => t.ordem)) + 1 : 0;
     const newTodo = {
-      localToDoId: Date.now().toString(),
-      localToDoItem: text,
-      localToDoOrdem: newOrder
+      id: Date.now().toString(),
+      item: text,
+      ordem: newOrder
     };
     todos.push(newTodo);
-    localStorage.setItem('localTodos', JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
     loadTodos();
 }           
 
 function loadTodos() {
     todoList.innerHTML = "";
-    const todos = JSON.parse(localStorage.getItem('localTodos')) || [];
-    todos.sort((a, b) => a.localToDoOrdem - b.localToDoOrdem).forEach((todoItem) => {
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    todos.sort((a, b) => a.ordem - b.ordem).forEach((todoItem) => {
         const todoElement = document.createElement("div");
         todoElement.className = "to-do " + (document.body.classList.contains("light-mode") ? "light-mode" : "dark-mode");
-        todoElement.dataset.localToDoId = todoItem.localToDoId;
+        todoElement.dataset.id = todoItem.id;
         const textDecoration = todoItem.done ? "text-decoration: line-through;" : "";
-        const checkIcon = todoItem.done ? `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-check-circle" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo checked" data-id="${todoItem.localToDoId}"><path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"/></svg>` : `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-circle-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo notChecked" data-id="${todoItem.localToDoId}"><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"/></svg>`; 
+        const checkIcon = todoItem.done ? `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-check-circle" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo checked" data-id="${todoItem.id}"><path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"/></svg>` : `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-circle-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo notChecked" data-id="${todoItem.id}"><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"/></svg>`; 
         
         todoElement.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-drag" width="21" height="21" viewBox="0 0 24 24" fill="currentColor" class="dragIcon"><path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z"/></svg>
-            <span style="width: 100%; ${textDecoration}">${todoItem.localToDoItem}</span>
+            <span style="width: 100%; ${textDecoration}">${todoItem.item}</span>
             <div class="toDoButtons">
             ${checkIcon}
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="deleteToDo" data-id="${todoItem.localToDoId}"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="deleteToDo" data-id="${todoItem.id}"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg>
             </div>
         `;
 
@@ -166,52 +169,33 @@ function loadTodos() {
 
 function updateTodoOrder() {
     const todoElements = todoList.querySelectorAll('.to-do');
-    const todos = JSON.parse(localStorage.getItem('localTodos')) || [];
-    const updatedTodos = [];
-
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
     todoElements.forEach((todoElement, index) => {
-        const todoId = todoElement.dataset.localToDoId;
-        const todo = todos.find(t => t.localToDoId === todoId);
-        if (todo) {
-            todo.localToDoOrdem = index;
-            updatedTodos.push(todo);
-        }
+      const todoId = todoElement.dataset.id;
+      const todo = todos.find(t => t.id === todoId);
+      if (todo) {
+        todo.ordem = index;
+      }
     });
-
-    localStorage.setItem('localTodos', JSON.stringify(updatedTodos));
-}
-
-function initializeTodoOrder() {
-    const todoElements = todoList.querySelectorAll('.to-do');
-    const todos = JSON.parse(localStorage.getItem('localTodos')) || [];
-    
-    todoElements.forEach((todoElement, index) => {
-        const todoId = todoElement.dataset.localToDoId;
-        const todo = todos.find(t => t.localToDoId === todoId);
-        if (todo) {
-            todo.localToDoOrdem = index;
-        }
-    });
-
-    localStorage.setItem('localTodos', JSON.stringify(todos));
-}
+    localStorage.setItem('todos', JSON.stringify(todos));
+}         
 
 function deleteTodo(todoId) {
-    let todos = JSON.parse(localStorage.getItem('localTodos')) || [];
-    todos = todos.filter(todo => todo.localToDoId !== todoId);
-    localStorage.setItem('localTodos', JSON.stringify(todos));
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
+    todos = todos.filter(todo => todo.id !== todoId);
+    localStorage.setItem('todos', JSON.stringify(todos));
     loadTodos();
 }
 
 function checkTodo(todoId) {
-    let todos = JSON.parse(localStorage.getItem('localTodos')) || [];
+    let todos = JSON.parse(localStorage.getItem('todos')) || [];
     todos = todos.map(todo => {
-        if (todo.localToDoId === todoId) {
+        if (todo.id === todoId) {
             todo.done = !todo.done;
         }
         return todo;
     });
-    localStorage.setItem('localTodos', JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos));
     loadTodos();
 }
 
@@ -245,8 +229,8 @@ function applyDarkMode() {
     botaoDark.classList.add('dark-mode');
     menu.classList.remove('light-mode');
     menu.classList.add('dark-mode');
-    sobre.classList.remove('light-mode');
-    sobre.classList.add('dark-mode');
+   sobre.classList.remove('light-mode');
+   sobre.classList.add('dark-mode');
     h3.style.color = "#DBDBDB";
     h32.style.color = "#DBDBDB";
     items.forEach(item => {
@@ -313,32 +297,32 @@ function applyLightMode() {
 
 // CRUD das notas
 function renderNotesList() {
-    localNotes.sort((a, b) => (a.localOrder || 0) - (b.localOrder || 0));
+    notes.sort((a, b) => (a.order || 0) - (b.order || 0));
     notesList.innerHTML = "";
     
-    localNotes.forEach(note => {
+    notes.forEach(note => {
         const li = document.createElement("li");
         li.innerHTML = `
         <div class="dragAndSpan">
             <div class="dragNote">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-drag" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z" /></svg>
             </div>
-                <span>${note.localTitle || "Bloco de notas"}</span>
+                <span>${note.title || "Bloco de notas"}</span>
         </div>
         <div class="note-actions">
             <button class="delete-note light-mode" title="Excluir nota"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg></button>
         </div>`;
-        li.dataset.id = note.localId;
+        li.dataset.id = note.id;
 
         const deleteButton = li.querySelector('.delete-note');
         deleteButton.addEventListener("click", (event) => {
             event.stopPropagation();
-            deleteNote(note.localId);
+            deleteNote(note.id);
         });
 
-        li.addEventListener("click", () => selectNote(note.localId));
+        li.addEventListener("click", () => selectNote(note.id));
 
-        if (note.localId === currentNoteId) {
+        if (note.id === currentNoteId) {
             li.classList.add("selected-note");
         }
 
@@ -360,9 +344,9 @@ function renderNotesList() {
 }
 
 function deleteNote(id) {
-    const note = localNotes.find(n => n.localId === id);
+    const note = notes.find(n => n.id === id);
     if (note) {
-        noteToDeleteSpan.textContent = note.localTitle || "Novo bloco";
+        noteToDeleteSpan.textContent = note.title || "Novo bloco";
         deleteConfirmation.classList.remove("hidden");
         currentNoteId = id;
     }
@@ -393,19 +377,19 @@ function initializeSidebarSortable() {
 
 function updateNotesOrder() {
     const noteElements = notesList.querySelectorAll('li');
-    localNotes = Array.from(noteElements).map((el, index) => {
-        const note = localNotes.find(note => note.localId === el.dataset.id);
-        return { ...note, localOrder: index };
+    notes = Array.from(noteElements).map((el, index) => {
+        const note = notes.find(note => note.id === el.dataset.id);
+        return { ...note, order: index };
     });
     saveNotes();
 }
 
 function selectNote(id) {
     currentNoteId = id;
-    const note = localNotes.find(n => n.localId === id);
+    const note = notes.find(n => n.id === id);
     if (note) {
-        noteTitle.value = note.localTitle || '';
-        noteBody.innerHTML = note.localBody || '';
+        noteTitle.value = note.title || '';
+        noteBody.innerHTML = note.body || '';
         home.classList.add("hidden");
         noteDetails.classList.remove("hidden");
 
@@ -422,19 +406,16 @@ function selectNote(id) {
 }
 
 function saveNote() {
-    if (!currentNoteId) return;
-    
-    const note = localNotes.find(n => n.localId === currentNoteId);
-    if (note) {
-        note.localTitle = noteTitle.value;
-        note.localBody = noteBody.innerHTML;
+    const note = {
+        id: currentNoteId,
+        title: noteTitle.value,
+        body: noteBody.innerHTML
+    };
+    const index = notes.findIndex(n => n.id === note.id);
+    if (index !== -1) {
+        notes[index] = note;
     } else {
-        localNotes.push({
-            localId: currentNoteId,
-            localTitle: noteTitle.value,
-            localBody: noteBody.innerHTML,
-            localOrder: localNotes.length
-        });
+        notes.push(note);
     }
     saveNotes();
     renderNotesList();
@@ -450,22 +431,22 @@ noteBody.addEventListener('paste', function(e) {
 
 function addNote() {
     const newNote = {
-        localId: Date.now().toString(),
-        localTitle: "Novo bloco",
-        localBody: "",
-        localOrder: localNotes.length
+        id: Date.now().toString(),
+        title: "Novo bloco",
+        body: "",
+        order: notes.length
     };
-    localNotes.push(newNote);
-    currentNoteId = newNote.localId;
+    notes.push(newNote);
+    currentNoteId = newNote.id;
     saveNotes();
     renderNotesList();
-    selectNote(newNote.localId);
+    selectNote(newNote.id);
 }
 
 addNoteButton.addEventListener("click", addNote);
 
 confirmDeleteButton.addEventListener("click", () => {
-    localNotes = localNotes.filter(note => note.localId !== currentNoteId);
+    notes = notes.filter(note => note.id !== currentNoteId);
     saveNotes();
     currentNoteId = null;
     deleteConfirmation.classList.add("hidden");
@@ -506,11 +487,11 @@ document.addEventListener('click', function(event) {
     }
 });
 
-function restoreSelectionAndApplyCommand(command) {
+function restoreSelectionAndApplyCommand(command, value = null) {
     const selection = window.getSelection();
     selection.removeAllRanges();
     selection.addRange(savedSelection);
-    document.execCommand(command, false, null);
+    document.execCommand(command, false, value);
     formatacao.style.display = 'none';
 }
 
@@ -538,6 +519,14 @@ rightBtn.addEventListener('click', function() {
     restoreSelectionAndApplyCommand('justifyRight');
 });
 
+justifyBtn.addEventListener('click', function() {
+    restoreSelectionAndApplyCommand('justifyFull');
+});
+
+listOrderBtn.addEventListener('click', function() {
+    restoreSelectionAndApplyCommand('insertOrderedList');
+});
+
 listBtn.addEventListener('click', function() {
     restoreSelectionAndApplyCommand('insertUnorderedList');
 });
@@ -562,6 +551,14 @@ linkBtn.addEventListener('click', function() {
     }
 });
 
+markerBtn.addEventListener('click', function() {
+    restoreSelectionAndApplyCommand('hiliteColor', 'yellow');
+});
+
+removeBtn.addEventListener('click', function() {
+    restoreSelectionAndApplyCommand('removeFormat');
+});
+
 function saveSelection() {
     const selection = getSelection();
     if (selection.rangeCount > 0) {
@@ -578,10 +575,6 @@ function restoreSelection(range) {
     }
 }
 
-removeBtn.addEventListener('click', function() {
-    restoreSelectionAndApplyCommand('removeFormat');
-});
-
 // Menu lateral
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle('active');
@@ -593,6 +586,5 @@ applyStoredTheme();
 initializeLocalStorage();
 renderNotesList();
 loadTodos();
-initializeTodoOrder();
 initializeSidebarSortable();
 loadingMessage.style.display = "none";
