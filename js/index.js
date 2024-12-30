a81c8728ca09d77a99bbf085dc804c853808df7f
Landowski <105506160+Landowski@users.noreply.github.com>
2024-12-30 18:18:07 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,8 +47,6 @@ const home = document.getElementById("home");
 const toggleDark = document.getElementById("dark-mode");
 const menu = document.getElementById("menu");
 const overlaySidebar = document.getElementById("overlay-sidebar");
-const todoInput = document.getElementById("input-to-do");
-const todoList = document.getElementById("to-do");
 const loginBtn = document.getElementById("loginBtn");
 const loginPopup = document.getElementById("login-popup");
 const popLogin = document.getElementById('popLogin');
@@ -91,17 +89,6 @@ loginBtn.addEventListener('click', function() {
     });
 });
 
-todoInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const todoText = todoInput.value.trim();
-        if (todoText !== "") {
-            addTodo(todoText);
-            todoInput.value = "";
-        }
-    }
-});
-
 toggleDark.addEventListener("click", () => {
     if (document.body.classList.contains('light-mode')) {
         applyDarkMode();
@@ -130,9 +117,6 @@ function initializeLocalStorage() {
     if (!localStorage.getItem('notes')) {
       localStorage.setItem('notes', JSON.stringify([]));
     }
-    if (!localStorage.getItem('todos')) {
-      localStorage.setItem('todos', JSON.stringify([]));
-    }
     syncNotes();
 }
 
@@ -151,117 +135,16 @@ function applyStoredTheme() {
     } else {
         applyLightMode();
     }
-}
-
-// Lista de tarefas
-function addTodo(text) {
-    const todos = JSON.parse(localStorage.getItem('todos')) || [];
-    const newOrder = todos.length > 0 ? Math.max(...todos.map(t => t.ordem)) + 1 : 0;
-    const newTodo = {
-      id: Date.now().toString(),
-      item: text,
-      ordem: newOrder
-    };
-    todos.push(newTodo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    loadTodos();
-}           
-
-function loadTodos() {
-    todoList.innerHTML = "";
-    const todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todos.sort((a, b) => a.ordem - b.ordem).forEach((todoItem) => {
-        const todoElement = document.createElement("div");
-        todoElement.className = "to-do " + (document.body.classList.contains("light-mode") ? "light-mode" : "dark-mode");
-        todoElement.dataset.id = todoItem.id;
-        const textDecoration = todoItem.done ? "text-decoration: line-through;" : "";
-        const checkIcon = todoItem.done ? `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-check-circle" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo checked" data-id="${todoItem.id}"><path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"/></svg>` : `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-circle-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="checkToDo notChecked" data-id="${todoItem.id}"><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"/></svg>`; 
-        
-        todoElement.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-drag" width="21" height="21" viewBox="0 0 24 24" fill="currentColor" class="dragIcon"><path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z"/></svg>
-            <span style="width: 100%; ${textDecoration}">${todoItem.item}</span>
-            <div class="toDoButtons">
-            ${checkIcon}
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor" class="deleteToDo" data-id="${todoItem.id}"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg>
-            </div>
-        `;
-
-        todoList.appendChild(todoElement);
-        
-        todoElement.querySelector(".deleteToDo").addEventListener("click", function() {
-            const todoId = this.getAttribute("data-id");
-            deleteTodo(todoId);
-        });
-
-        todoElement.querySelector(".checkToDo").addEventListener("click", function() {
-            const todoId = this.getAttribute("data-id");
-            checkTodo(todoId);
-        });
-    });
-
-    new Sortable(todoList, {
-        handle: '.dragIcon',
-        animation: 150,
-        onStart: function(evt) {
-            const todos = todoList.querySelectorAll('.to-do');
-            todos.forEach((todo, index) => {
-                if (index !== evt.oldIndex) {
-                    todo.classList.add('faded');
-                }
-            });
-        },
-        onEnd: function(evt) {
-            const todos = todoList.querySelectorAll('.to-do');
-            todos.forEach((todo) => {
-                todo.classList.remove('faded');
-            });
-            updateTodoOrder();
-        }
-    });
-}
-
-function updateTodoOrder() {
-    const todoElements = todoList.querySelectorAll('.to-do');
-    const todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todoElements.forEach((todoElement, index) => {
-      const todoId = todoElement.dataset.id;
-      const todo = todos.find(t => t.id === todoId);
-      if (todo) {
-        todo.ordem = index;
-      }
-    });
-    localStorage.setItem('todos', JSON.stringify(todos));
-}         
-
-function deleteTodo(todoId) {
-    let todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todos = todos.filter(todo => todo.id !== todoId);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    loadTodos();
-}
-
-function checkTodo(todoId) {
-    let todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todos = todos.map(todo => {
-        if (todo.id === todoId) {
-            todo.done = !todo.done;
-        }
-        return todo;
-    });
-    localStorage.setItem('todos', JSON.stringify(todos));
-    loadTodos();
-}
+}          
 
 // Dark mode
 function applyDarkMode() {
     const items = document.querySelectorAll('#sidebar ul li');
     const botoes = document.querySelectorAll('.note-actions button');
-    const toDoText = document.querySelectorAll('.to-do');
     const botaoDark = document.getElementById('dark-mode');
     const h3 = document.getElementById('h3');
     const h32 = document.getElementById('h3-2');
     const sidebar = document.getElementById('sidebar');
-    const insertToDo = document.querySelector(".insertToDo");
     const notebody = document.getElementById('note-body');
     const textEditor = document.getElementById('text-editor');
     const sobre = document.getElementById('sobre');
@@ -270,8 +153,6 @@ function applyDarkMode() {
     document.body.classList.add('dark-mode');
     notebody.classList.remove('light-mode');
     notebody.classList.add('dark-mode');
-    insertToDo.classList.remove('light-mode');
-    insertToDo.classList.add('dark-mode');
     textEditor.classList.remove('light-mode');
     textEditor.classList.add('dark-mode');
     sidebar.classList.remove('light-mode');
@@ -294,21 +175,15 @@ function applyDarkMode() {
         item.classList.remove('light-mode');
         item.classList.add('dark-mode');
     });
-    toDoText.forEach(item => {
-        item.classList.remove('light-mode');
-        item.classList.add('dark-mode');
-    });
 }
 
 function applyLightMode() {
     const items = document.querySelectorAll('#sidebar ul li');
     const botoes = document.querySelectorAll('.note-actions button');
-    const toDoText = document.querySelectorAll('.to-do');
     const botaoDark = document.getElementById('dark-mode');
     const h3 = document.getElementById('h3');
     const h32 = document.getElementById('h3-2');
     const sidebar = document.getElementById('sidebar');
-    const insertToDo = document.querySelector(".insertToDo");
     const notebody = document.getElementById('note-body');
     const textEditor = document.getElementById('text-editor');
     const sobre = document.getElementById('sobre');
@@ -317,8 +192,6 @@ function applyLightMode() {
     document.body.classList.add('light-mode');
     notebody.classList.remove('dark-mode');
     notebody.classList.add('light-mode');
-    insertToDo.classList.remove('dark-mode');
-    insertToDo.classList.add('light-mode');
     textEditor.classList.remove('dark-mode');
     textEditor.classList.add('light-mode');
     sidebar.classList.remove('dark-mode');
@@ -338,10 +211,6 @@ function applyLightMode() {
         item.classList.add('light-mode');
     });
     botoes.forEach(item => {
-        item.classList.remove('dark-mode');
-        item.classList.add('light-mode');
-    });
-    toDoText.forEach(item => {
         item.classList.remove('dark-mode');
         item.classList.add('light-mode');
     });
@@ -803,6 +672,5 @@ document.getElementById('linkEsqueci').addEventListener('click', () => {
 applyStoredTheme();
 initializeLocalStorage();
 renderNotesList();
-loadTodos();
 initializeSidebarSortable();
 loadingMessage.style.display = "none";
