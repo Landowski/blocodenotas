c4ec0f2a98efb0b810987bd2c70f4c1d33ac69b1
Landowski <105506160+Landowski@users.noreply.github.com>
2025-02-20 14:10:05 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     storageBucket: "bloco-de-notas-b94c4.appspot.com",
     messagingSenderId: "515098342854",
     appId: "1:515098342854:web:4b91c903046400c7f96ec7"
-};
+  };
 
 // Inicia o Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,151 +18,156 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const userDocRef = db.collection('usuario').doc(user.uid);
                         
             userDocRef.get().then((doc) => {
-                // Dados do usuário
-                const userData = doc.exists ? doc.data() : {};
+                if (doc.exists) {
                 
-                // Declarações
-                const nome = document.getElementById('nome')
-                const logo = document.querySelector(".logo");
-                const notesList = document.getElementById("notes-list");
-                const loadingMessage = document.getElementById("loading-message");
-                const addNoteButton = document.getElementById("add");
-                const noteDetails = document.getElementById("note-details");
-                const noteTitle = document.getElementById("note-title");
-                const noteBody = document.getElementById("note-body");
-                const formatacao = document.getElementById('formatacao');
-                const boldBtn = document.getElementById('boldBtn');
-                const italicBtn = document.getElementById('italicBtn');
-                const underlineBtn = document.getElementById('underlineBtn');
-                const markerBtn = document.getElementById('markerBtn');
-                const listOrderBtn = document.getElementById('listOrderBtn');
-                const listBtn = document.getElementById('listBtn');
-                const linkBtn = document.getElementById('linkBtn');
-                const removeBtn = document.getElementById('removeBtn');
-                const deleteConfirmation = document.getElementById("delete-confirmation");
-                const confirmDeleteButton = document.getElementById("confirm-delete");
-                const cancelDeleteButton = document.getElementById("cancel-delete");
-                const noteToDeleteSpan = document.getElementById("note-to-delete");
-                const home = document.getElementById("home");
-                const toggleDark = document.getElementById("dark-mode");
-                const menu = document.getElementById("menu");
-                const overlaySidebar = document.getElementById("overlay-sidebar");
-                const configBtn = document.getElementById("config");
-                const configPopup = document.getElementById("config-popup");
-                const nomeInput = document.getElementById('nome-input');
-                const emailInput = document.getElementById('email-input');
-                const closeConfig = document.getElementById('close-config');
-                const email = document.getElementById('email');
-                let currentNoteId = null;
+                    // remove a barreira
+                    document.getElementById('barreira').style.display = 'none';
 
-                // Nome e e-mail do usuário
-                nome.textContent = userData.nome || '';
-                email.textContent = user.email;
-
-                // Listeners iniciais
-                menu.addEventListener('click', toggleSidebar);
-                overlaySidebar.addEventListener('click', toggleSidebar);
-                configBtn.addEventListener('click', openConfig);
-                configPopup.addEventListener('click', closeConfigPop);
-                closeConfig.addEventListener('click', closeConfigPop);
-
-                toggleDark.addEventListener("click", () => {
-                    if (document.body.classList.contains('light-mode')) {
-                        applyDarkMode();
-                        userDocRef.update({
-                            modo_escuro: true
-                        });
-                    } else {
-                        applyLightMode();
-                        userDocRef.update({
-                            modo_escuro: false
-                        });
-                    }
-                });
-
-                logo.addEventListener("click", () => {
-                    if (currentNoteId) {
-                        saveNote();
-                    }
-                    noteDetails.classList.add("hidden");
-                    home.classList.remove("hidden");
-                    const selectedNote = notesList.querySelector(".selected-note");
-                    if (selectedNote) {
-                        selectedNote.classList.remove("selected-note");
-                    }
-                    currentNoteId = null;
-                });
-
-                function applyStoredTheme() {
-                    if (userData.modo_escuro) {
-                        applyDarkMode();
-                    } else {
-                        applyLightMode();
-                    }
-                }
-                
-                // CRUD do bloco de notas
-                function loadNotes() {             
-                    const db = firebase.firestore();
+                    // Dados do usuário
+                    const userData = doc.data();
                     
-                    db.collection('bloco')
-                        .where('usuario', '==', user.uid)
-                        .orderBy('ordem', 'asc')
-                        .get()
-                        .then((querySnapshot) => {
-                            notesList.innerHTML = "";
-                            
-                            querySnapshot.forEach((doc) => {
-                                const note = {
-                                    id: doc.id,
-                                    ...doc.data()
-                                };
-                
-                                const li = document.createElement("li");
-                                li.innerHTML = `
-                                    <div class="dragAndSpan">
-                                        <div class="dragNote">
-                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-drag" width="21" height="21" viewBox="0 0 24 24" fill="currentColor"><path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z" /></svg>
-                                        </div>
-                                        <span>${note.titulo || "Bloco de notas"}</span>
-                                    </div>
-                                    <div class="note-actions">
-                                        <button class="delete-note light-mode" title="Excluir nota"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg></button>
-                                    </div>`;
-                                li.dataset.id = note.id;
-                
-                                const deleteButton = li.querySelector('.delete-note');
-                                deleteButton.addEventListener("click", (event) => {
-                                    event.stopPropagation();
-                                    deleteNote(note.id);
-                                });
-                
-                                li.addEventListener("click", () => selectNote(note.id));
-                
-                                if (note.id === currentNoteId) {
-                                    li.classList.add("selected-note");
-                                }
-                
-                                notesList.appendChild(li);
+                    // Declarações
+                    const nome = document.getElementById('nome')
+                    const logo = document.querySelector(".logo");
+                    const notesList = document.getElementById("notes-list");
+                    const loadingMessage = document.getElementById("loading-message");
+                    const addNoteButton = document.getElementById("add");
+                    const noteDetails = document.getElementById("note-details");
+                    const noteTitle = document.getElementById("note-title");
+                    const noteBody = document.getElementById("note-body");
+                    const formatacao = document.getElementById('formatacao');
+                    const boldBtn = document.getElementById('boldBtn');
+                    const italicBtn = document.getElementById('italicBtn');
+                    const underlineBtn = document.getElementById('underlineBtn');
+                    const markerBtn = document.getElementById('markerBtn');
+                    const listOrderBtn = document.getElementById('listOrderBtn');
+                    const listBtn = document.getElementById('listBtn');
+                    const linkBtn = document.getElementById('linkBtn');
+                    const removeBtn = document.getElementById('removeBtn');
+                    const deleteConfirmation = document.getElementById("delete-confirmation");
+                    const confirmDeleteButton = document.getElementById("confirm-delete");
+                    const cancelDeleteButton = document.getElementById("cancel-delete");
+                    const noteToDeleteSpan = document.getElementById("note-to-delete");
+                    const home = document.getElementById("home");
+                    const toggleDark = document.getElementById("dark-mode");
+                    const menu = document.getElementById("menu");
+                    const overlaySidebar = document.getElementById("overlay-sidebar");
+                    const configBtn = document.getElementById("config");
+                    const configPopup = document.getElementById("config-popup");
+                    const nomeInput = document.getElementById('nome-input');
+                    const emailInput = document.getElementById('email-input');
+                    const closeConfig = document.getElementById('close-config');
+                    const email = document.getElementById('email');
+                    let currentNoteId = null;
+
+                    // Nome e e-mail do usuário
+                    nome.textContent = userData.nome;
+                    email.textContent = user.email;
+
+                    // Listeners iniciais
+                    menu.addEventListener('click', toggleSidebar);
+                    overlaySidebar.addEventListener('click', toggleSidebar);
+                    configBtn.addEventListener('click', openConfig);
+                    configPopup.addEventListener('click', closeConfigPop);
+                    closeConfig.addEventListener('click', closeConfigPop);
+
+                    toggleDark.addEventListener("click", () => {
+                        if (document.body.classList.contains('light-mode')) {
+                            applyDarkMode();
+                            userDocRef.update({
+                                modo_escuro: true
                             });
-                
-                            const items = document.querySelectorAll('#sidebar ul li');
-                            if (document.body.classList.contains('light-mode')) {
-                                items.forEach(item => {
-                                    item.classList.add('light-mode');
+                        } else {
+                            applyLightMode();
+                            userDocRef.update({
+                                modo_escuro: false
+                            });
+                        }
+                    });
+
+                    logo.addEventListener("click", () => {
+                        if (currentNoteId) {
+                            saveNote();
+                        }
+                        noteDetails.classList.add("hidden");
+                        home.classList.remove("hidden");
+                        const selectedNote = notesList.querySelector(".selected-note");
+                        if (selectedNote) {
+                            selectedNote.classList.remove("selected-note");
+                        }
+                        currentNoteId = null;
+                    });
+
+                    function applyStoredTheme() {
+                        if (userData.modo_escuro) {
+                            applyDarkMode();
+                        } else {
+                            applyLightMode();
+                        }
+                    }
+                    
+                    // CRUD do bloco de notas
+                    function loadNotes() {             
+                        const db = firebase.firestore();
+                        
+                        db.collection('bloco')
+                            .where('usuario', '==', user.uid)
+                            .orderBy('ordem', 'asc')
+                            .get()
+                            .then((querySnapshot) => {
+                                notesList.innerHTML = "";
+                                
+                                querySnapshot.forEach((doc) => {
+                                    const note = {
+                                        id: doc.id,
+                                        ...doc.data()
+                                    };
+                    
+                                    const li = document.createElement("li");
+                                    li.innerHTML = `
+                                        <div class="dragAndSpan">
+                                            <div class="dragNote">
+                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-drag" width="21" height="21" viewBox="0 0 24 24" fill="currentColor"><path d="M7,19V17H9V19H7M11,19V17H13V19H11M15,19V17H17V19H15M7,15V13H9V15H7M11,15V13H13V15H11M15,15V13H17V15H15M7,11V9H9V11H7M11,11V9H13V11H11M15,11V9H17V11H15M7,7V5H9V7H7M11,7V5H13V7H11M15,7V5H17V7H15Z" /></svg>
+                                            </div>
+                                            <span>${note.titulo || "Bloco de notas"}</span>
+                                        </div>
+                                        <div class="note-actions">
+                                            <button class="delete-note light-mode" title="Excluir nota"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-trash-can-outline" width="19" height="19" viewBox="0 0 24 24" fill="currentColor"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"/></svg></button>
+                                        </div>`;
+                                    li.dataset.id = note.id;
+                    
+                                    const deleteButton = li.querySelector('.delete-note');
+                                    deleteButton.addEventListener("click", (event) => {
+                                        event.stopPropagation();
+                                        deleteNote(note.id);
+                                    });
+                    
+                                    li.addEventListener("click", () => selectNote(note.id));
+                    
+                                    if (note.id === currentNoteId) {
+                                        li.classList.add("selected-note");
+                                    }
+                    
+                                    notesList.appendChild(li);
                                 });
-                            } else {
-                                items.forEach(item => {
-                                    item.classList.add('dark-mode');
-                                });
-                            }
-                
-                            initializeSidebarSortable();
-                        })
-                        .catch((error) => {
-                            console.error("Erro ao carregar notas:", error);
-                        });
-                }
+                    
+                                const items = document.querySelectorAll('#sidebar ul li');
+                                if (document.body.classList.contains('light-mode')) {
+                                    items.forEach(item => {
+                                        item.classList.add('light-mode');
+                                    });
+                                } else {
+                                    items.forEach(item => {
+                                        item.classList.add('dark-mode');
+                                    });
+                                }
+                    
+                                initializeSidebarSortable();
+                            })
+                            .catch((error) => {
+                                console.error("Erro ao carregar notas:", error);
+                            });
+                    }
                     
                     function deleteNote(id) {
                         const db = firebase.firestore();
@@ -571,15 +576,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     });
 
                     // Inicialização
-                applyStoredTheme();
-                loadNotes();
-                initializeSidebarSortable();
-                loadingMessage.style.display = "none";
+                    applyStoredTheme();
+                    loadNotes();
+                    initializeSidebarSortable();
+                    loadingMessage.style.display = "none";
 
-                document.getElementById('sair').addEventListener('click', function() {
-                    firebase.auth().signOut();
-                });
+                    document.getElementById('sair').addEventListener('click', function() {
+                        firebase.auth().signOut();
+                    });
             });
+
         } else {
             window.location.href = '/';
         }
