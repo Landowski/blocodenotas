d224f2a34ba3f0cd1c63fa3a5d90104d9d2c3f38
Landowski <105506160+Landowski@users.noreply.github.com>
2025-03-04 12:22:23 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -67,7 +67,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     menu.addEventListener('click', toggleSidebar);
                     overlaySidebar.addEventListener('click', toggleSidebar);
                     configBtn.addEventListener('click', openConfig);
-                    configPopup.addEventListener('click', closeConfigPop);
                     closeConfig.addEventListener('click', closeConfigPop);
 
                     toggleDark.addEventListener("click", () => {
